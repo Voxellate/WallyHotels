438769557e6a90b3b7944151867a7bbaaa4d77b0
Lucas Dugdale <LukeDugdale@icloud.com>
2017-10-12 19:26:26 +1000

    added youtube api

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Lucas on 10/12/2017.
+ */
+function scrollto (x) {
+    document.getElementById(x).scrollIntoView();
+}
+
+function validateForm () {
+    var x = document.forms["mapsearch"]["postcode"].value;
+    if (isNaN(x) || x < 1000 || x > 9999) {
+        document.getElementById("postcode").innerHTML = "Please enter a valid postcode:";
+        return false;
+    } else {
+        document.getElementById("postcode").innerHTML = "Postcode:";
+    }
+}
